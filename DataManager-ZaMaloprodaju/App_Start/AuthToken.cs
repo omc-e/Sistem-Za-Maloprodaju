2093cee931fe21dc88451a361497cac2752f581a
Emsar Omić <emsar.omic@outlook.com>
2022-06-06 14:17:26 +0200

    Add Swagger Documentation to API
    Added 2 classes; 1 to add token endpoint, 1 to allow for extra parameter for every method to allow to put token into it

---
+++ DataManager-ZaMaloprodaju/App_Start/AuthToken.cs
@@ -0,0 +1,51 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace DataManager_ZaMaloprodaju.App_Start
+{
+    public class AuthToken : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {   
+                    tags = new List<string> { "Auth" },
+                    //What type of type of data
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    //definition of paramteres, we get boxes to fill in grant_type, username, password
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
