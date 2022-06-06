bdf414b9414baf46833976c03bed190a24d4e201
Emsar Omić <emsar.omic@outlook.com>
2022-06-06 16:06:14 +0200

    Creatim MVVM
    Adding ShellViewModel and ShellView for my application
    Adding Bootstrapper

---
+++ Sistem-Za-Maloprodaju-WPFUserInterface/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.Micro;
+using Sistem_Za_Maloprodaju_WPFUserInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sistem_Za_Maloprodaju_WPFUserInterface
+{
+    public class Bootstrapper : BootstrapperBase 
+    {
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+
+            protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+
+
+    }
+    
+
