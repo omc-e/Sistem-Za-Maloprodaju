44df86d22bb6de65869cb229c326078cba6cf84a
Emsar Omić <emsar.omic@outlook.com>
2022-07-14 12:13:45 +0200

    Sales Page
    Adds the sales page XAML and the supporting
    View Model. Creating template in XAML and ViewModel.

---
+++ Sistem-Za-Maloprodaju-WPFUserInterface/ViewModels/SalesViewModel.cs
@@ -0,0 +1,130 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistem_Za_Maloprodaju_WPFUserInterface.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set { _products = value; 
+            NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private string _itemQuantity;
+
+        public  string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set { _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+                    }
+        }
+
+        
+
+        public string  SubTotal
+        {
+            get {
+                return "$0.00";
+            }
+            
+        }
+
+        public string Tax
+        {
+            get
+            {
+                return "$0.00";
+            }
+
+        }
+        public string Total
+        {
+            get
+            {
+                return "$0.00";
+            }
+
+        }
+
+
+        private BindingList<string> _cart;
+
+        public  BindingList<string> Cart
+        {
+            get { return _cart; }
+            set { _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+
+            }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+                
+                //Make sure something is selected
+                //Make sure there is an item quantity
+                
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+                
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //Make there is something in the cart
+
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
+
+
+
+    }
+}
