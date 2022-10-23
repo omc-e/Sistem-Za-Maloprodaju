0ee9e837e48f745a7d885ae44c284b77469cbf04
omc-e <emsar.omic@outlook.com>
2022-10-23 11:57:07 +0200

    Add Foreign Key Relationships

--- Sistem-Za-Maloprodaju-DB/dbo/Tables/SaleDetail.sql
+++ Sistem-Za-Maloprodaju-DB/dbo/Tables/SaleDetail.sql
@@ -2,8 +2,10 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [SaleId] INT NOT NULL, 
-    [ProducitID ] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1,
     [PurchasePrice] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL DEFAULT 0 
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES [Sale](Id), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]) 
 )
