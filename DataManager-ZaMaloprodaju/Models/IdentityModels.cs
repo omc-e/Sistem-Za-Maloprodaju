6ff1af208afcd38c8efbb2fe2154cfd0bcb5dc91
omc-e <emsar.omic@outlook.com>
2022-10-23 12:12:46 +0200

    Update EF Database Name to EFData

--- DataManager-ZaMaloprodaju/Models/IdentityModels.cs
+++ DataManager-ZaMaloprodaju/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace DataManager_ZaMaloprodaju.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
