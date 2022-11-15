043ead38e06284d60a50742a7cc31e3da305e3f3
omc-e <emsar.omic@outlook.com>
2022-11-15 21:20:18 +0100

    Properties and endpoints to add and remove user role
    Adds endpoints to add and remove user role, modyfing user controler and user endpoint and editing User Managment Display View

--- DesktopUI.Library/API/IUserEndPoint.cs
+++ DesktopUI.Library/API/IUserEndPoint.cs
@@ -7,5 +7,8 @@ namespace DesktopUI.Library.API
     public interface IUserEndPoint
     {
         Task<List<UserModel>> GetAll();
+        Task<Dictionary<string, string>> GetAllRoles();
+        Task AddUserToRole(string userId, string roleName);
+        Task RemoveUserFromRole(string userId, string roleName);
     }
 }
--- Sistem-Za-Maloprodaju-WPFUserInterface/ViewModels/UserDisplayViewModel.cs
+++ Sistem-Za-Maloprodaju-WPFUserInterface/ViewModels/UserDisplayViewModel.cs
@@ -34,6 +34,81 @@ namespace Sistem_Za_Maloprodaju_WPFUserInterface.ViewModels
             }
         }
 
+        private UserModel _selectedUser;
+
+        public UserModel SelectedUser
+        {
+            get { return _selectedUser; }
+            set { _selectedUser = value; 
+                SelectedUserName = value.Email;
+                UserRoles.Clear();
+                UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+                LoadRoles();
+            NotifyOfPropertyChange(() => SelectedUser);
+            }
+        }
+
+        private string _selectedUserRole;
+        public string SelectedUserRole
+        {
+            get { return _selectedUserRole; }
+            set
+            {
+                _selectedUserRole = value;
+                NotifyOfPropertyChange(() => SelectedUserRole);
+                
+            }
+        }
+
+        private string _selectedAvailableRole;
+        public string SelectedAvailableRole
+        {
+            get { return _selectedAvailableRole; }
+            set
+            {
+                _selectedAvailableRole = value;
+                NotifyOfPropertyChange(() => SelectedAvailableRole);
+                
+            }
+        }
+
+        private string _selectedUserName;
+
+        public string SelectedUserName
+        {
+            get { 
+                return _selectedUserName;
+            }
+            set { 
+                _selectedUserName = value;
+                NotifyOfPropertyChange(() => SelectedUserName);
+            }
+        }
+
+        private BindingList<string> _userRoles = new BindingList<string>();
+
+        public BindingList<string> UserRoles
+        {
+            get { return _userRoles; }
+            set { _userRoles = value;
+
+                NotifyOfPropertyChange(() => UserRoles);
+            }
+        }
+
+        private BindingList<string> _availableRoles = new BindingList<string>();
+
+        public BindingList<string> AvailableRoles
+        {
+            get { return _availableRoles; }
+            set
+            {
+                _availableRoles = value;
+
+                NotifyOfPropertyChange(() => AvailableRoles);
+            }
+        }
+
         public UserDisplayViewModel(StatusInfoViewModel status, IWindowManager window, IUserEndPoint userEndPoint)
         {
             _status = status;
@@ -77,6 +152,34 @@ namespace Sistem_Za_Maloprodaju_WPFUserInterface.ViewModels
             var userList = await _userEndPoint.GetAll();
             Users = new BindingList<UserModel>(userList);
             
+        }
+
+        private async Task LoadRoles()
+        {
+            var roles = await _userEndPoint.GetAllRoles();
+            foreach (var role in roles)
+            {
+                if(UserRoles.IndexOf(role.Value) < 0)
+                {
+                    AvailableRoles.Add(role.Value);
+                }
+            }
+        }
+
+        public async void AddSelectedRole()
+        {
+            await   _userEndPoint.AddUserToRole(SelectedUser.Id, SelectedAvailableRole);
+
+            UserRoles.Add(SelectedAvailableRole);
+            AvailableRoles.Remove(SelectedAvailableRole);
+        }
+
+        public async void RemoveSelectedRole()
+        {
+            await _userEndPoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+
+            AvailableRoles.Add(SelectedUserRole);
+            UserRoles.Remove(SelectedUserRole);
         }
     }
 }
